897502a4874058dccbb8d82663fe7e26fbc434d7
Aymeric <aymeric.gioia@hotmail.fr>
2017-01-06 15:34:03 +0100

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('have fun !');
+$("input").keypress(function(press){
+	var prenom = $("#first_name").val();
+	var nom = $("#last_name").val();
+	var ville = $("#city").val();
+
+	if (press.which == 13) {
+		console.log( " Je m'appelle " + prenom + " " + 	nom + " et j'habite à " + ville + " . ");
+	}
+});
